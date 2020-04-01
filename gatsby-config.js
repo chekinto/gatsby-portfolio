84ddd0abf81dad114aa01d2c95e1d381c840bf3f
Che To <chekinto2@gmail.com>
2020-04-01 19:39:23 +0100

    installed started dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,12 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: true,
+      },
+    },
+  ],
 }
